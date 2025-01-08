116ff462ac47a70a7caa024c4728519276f1f357
Regera24 <regera241@gmail.com>
2025-01-08 19:49:07 +0700

    Khoi tao du an

---
+++ swp391/src/main/java/org/group5/swp391/Swp391Application.java
@@ -0,0 +1,13 @@
+package org.group5.swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391Application.class, args);
+    }
+
+}
